ce9c2d41c63b1c8ee0f526a578ddee5727df6986
sdmlr <sdmlr_@hotmail.com>
2024-10-15 12:30:09 +0100

    feedback implemented, added test

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,16 +1,20 @@
 const { fetchArticleById } = require('../models/articles.model')
 
 exports.getArticleById = (req, res, next) => {
-    console.log(req)
     const { article_id } = req.params;
+    const regex = /^\d+$/;
+
+    if (!regex.test(article_id)) {
+        return res.status(400).send({ msg: 'Invalid ID format'})
+    }
+
 
     fetchArticleById(article_id)
         .then(result => {
-            console.log(result.rows.length)
             if (result.rows.length === 0) {
                 return res.status(404).send({ msg: 'Article not found' })
             }
-            res.status(200).send(result.rows[0]);
+            res.status(200).send({ article: result.rows[0] });
         })
         .catch(err => {
             console.error(err)
